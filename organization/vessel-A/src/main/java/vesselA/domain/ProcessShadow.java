bf8b54e639df0ba5c3d137130862a679d422f274
i-qiqi <1067967678@qq.com>
2018-07-26 19:48:25 +0800

    push

--- organization/vessel-A/src/main/java/vesselA/domain/ProcessShadow.java
+++
@@ -1,9 +0,0 @@
-package vesselA.domain;
-
-import lombok.Data;
-
-@Data
-public class ProcessShadow {
-    private String pid;
-    private String Status;
-}
--- organization/vessel-A/src/main/java/vesselA/repos/ProcessShadowRepository.java
+++
@@ -1,9 +0,0 @@
-package vesselA.repos;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ProcessShadowRepository {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-}
